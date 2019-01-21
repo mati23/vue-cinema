d44cfd799e396f55181b9005cdebee1a3ddf2e19
macedo <jose.macedo@3eengenharia.com.br>
2019-01-21 15:05:19 -0300

    PET-13

--- src/components/crudCinema/Createcinema.vue
+++ src/components/crudCinema/Createcinema.vue
@@ -1,25 +1,25 @@
 <template>
   <form>
     <v-text-field
-      v-model="cineNome"
+      v-model="cinema.nome"
       label="Cinema"
       required
     ></v-text-field>
     <v-select
-      v-model="uf"
+      v-model="cinema.estado"
       :items="uflist"
       label="Estado"
       required
       @change="listarcidades"
     ></v-select>
     <v-select
-    v-model="cidad"
+    v-model="cinema.cidade"
       :items="cidadeslist"
       label="Cidade"
       required
     ></v-select>
     <v-text-field
-      v-model="cineLocalizacao"
+      v-model="cinema.localizacao"
       label="Localização"
       required
     ></v-text-field>
@@ -28,23 +28,55 @@
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
 
   export default {
     data: () => ({
-      cineNome:'', cineLocalizacao:'', cidad:'',
+      cinema:{
+        _id:'',
+        _rev:'',
+        collection:'',
+        nome:'',
+        estado:'',
+        cidade:'',
+        localizacao:''
+      },
       uflist: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
       'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO',
-      'RR', 'SC', 'SP', 'SE', 'TO'], 
-      cidadeslist:[],
-      uf: ''
+      'RR', 'SC', 'SP', 'SE', 'TO'],
+      cidadeslist:[]
     }),
+    mounted(){
+      if(this.$route.params.id){
+        this.$axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/ingresso_online/_find',{
+          selector:{
+            "_id": this.$route.params.id
+          },
+        }).then(resultado => {                
+          this.cinema = resultado.data.docs[0]
+          this.listarcidades()
+        }).catch(error => console.log(error))
+      }
+    },
     methods:{
+        lercadastro(){
+        this.$nextTick(()=>{
+          axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/trg_municipios/_find',{
+            "selector": {
+              "sigla": this.cinema.estado
+            },fields:["cidades"]
+          }).then(resultado => { 
+            this.cidadeslist = resultado.data.docs["0"].cidades
+          }).catch(function (error){
+            console.log(error);
+          });
+        })
+      },
       listarcidades(){
         this.$nextTick(()=>{
           axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/trg_municipios/_find',{
             "selector": {
-              "sigla": this.uf
+              "sigla": this.cinema.estado
             },fields:["cidades"]
           }).then(resultado => { 
             this.cidadeslist = resultado.data.docs["0"].cidades
@@ -54,13 +86,25 @@ import axios from 'axios'
         })
       },
       salvarcadastro(){
-        this.$axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/ingresso_online/',{
+        if(this.$route.params.id){
+          this.$axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/ingresso_online/',{
+            "_id": this.cinema._id,
+            "_rev": this.cinema._rev,
+            "collection": this.cinema.collection,
+            "nome": this.cinema.nome,
+            "estado": this.cinema.estado,
+            "cidade": this.cinema.cidade,
+            "localizacao": this.cinema.localizacao
+          })
+        }else{
+          this.$axios.post('http://admin:admin2435,@couch-dev.3e.eng.br:5984/ingresso_online/',{
           "collection": "cinema",
-          "nome": this.cineNome,
-          "estado": this.uf,
-          "cidade": this.cidad,
-          "localizacao": this.cineLocalizacao
+          "nome": this.cinema.nome,
+          "estado": this.cinema.estado,
+          "cidade": this.cinema.cidade,
+          "localizacao": this.cinema.localizacao
         })
+        }
       }
     }
   }
--- src/components/crudCinema/Readcinema.vue
+++ src/components/crudCinema/Readcinema.vue
@@ -18,52 +18,27 @@
             target="">Cadastrar</v-btn>
         <v-card>
           <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
+            <span class="headline"></span>
           </v-card-title>
-
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.name" label="Nome"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.calories" label="Estado"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.fat" label="Cidade"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.fat" label="Localização"></v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-          </v-card-actions>
         </v-card>
       </v-dialog>
     </v-toolbar>
     <v-data-table
       :headers="headers"
-      :items="filmes"
+      :items="cinemas"
       class="elevation-1"
     >
       <template slot="items" slot-scope="props">
-        <td>{{ props.item.titulo }}</td>
-        <td class="text-xs-right">{{ props.item.genero }}</td>
-        <td class="text-xs-right">{{ props.item.classificacao }}</td>
+        <td>{{ props.item.nome }}</td>
+        <td class="text-xs-right">{{ props.item.cidade }}</td>
+        <td class="text-xs-right">{{ props.item.localizacao }}</td>
         <td class="justify-center layout px-0">
           <v-icon
             small
             class="mr-2"
-            @click="editItem(props.item)"
+            @click="editItem(props.item._id)"
           >
-            add
+            edit
           </v-icon>
           <v-icon
             small
@@ -82,88 +57,46 @@
 
 
 <script>
+  import Createcinema from './Createcinema.vue';
+  import Vue from 'vue'
+  import Router from '../../router'
+
   export default {
     data: () => ({
-      filmes: [],
+      cinemas: [],
       dialog: false,
       headers: [
         {
           text: 'Nome',
           align: 'left',
           sortable: false,
-          value: ''
+          value: 'nome'
         },
         { text: 'Estado', value: '', sortable: false },
         { text: 'Cidade', value: '', sortable: false },        
         { text: 'Localização', value: '', sortable: false }
       ],
-      editedIndex: -1,
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      }
     }),
     mounted(){
         this.$axios.post(
             'http://admin:admin2435,@couch-dev.3e.eng.br:5984/ingresso_online/_find',{selector:{
-                "collection": "filmes"
-            },fields: ["_id","titulo", "genero","classificacao"]
+                "collection": "cinema"
+            },fields: ["_id","nome", "cidade","localizacao"]
             }
             ).then(resultado => {                
-                this.filmes = resultado.data.docs           
+                this.cinemas = resultado.data.docs           
             }).catch(error => console.log(error))
-    },
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      }
-    },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
     },
 
     methods: {
       editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
+        this.$router.push({name: 'cadastro_cinema', params: {id: item}})
       },
 
       deleteItem (item) {
         const index = this.desserts.indexOf(item)
         confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
       },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
-      }
     }
   }
 </script>
